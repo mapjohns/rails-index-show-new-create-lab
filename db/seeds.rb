b9dc891bea6b19b15a5280480a122cf1a46566d1
Matthew <mattj20@gmail.com>
2022-07-12 07:35:04 -0700

    commit tree 6d54061c19eb65ec59d40c352307eb9f1d6fd95a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Coupon.create(coupon_code: "ASDKJH123" , store: "Choltes")
+Coupon.create(coupon_code: "HGNJAK6985" , store: "EV Games")
